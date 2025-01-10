741e03f0b8fd0c486fe810bccb91bc8f47128d8a
lenanasirov <lenanasirov@gmail.com>
2025-01-10 14:52:48 +0200

    adding more comments

--- hw3.py
+++ hw3.py
@@ -137,46 +137,59 @@ class DBManager:
         # Retrieve the updated user to check the rented_game_ids list
         updated_user = self.user_collection.find_one({"_id": user["_id"]})
 
-        #get the list of games rented by the user
+        # Get the list of games rented by the user
         rented_game_ids_list = updated_user.get("rented_game_ids", [])
+
+        # If the user hasn't rented any games, return a message indicating no games rented
         if not rented_game_ids_list:
             return ["No games rented"]
+        
+        # Fetch all the rented game documents from the game collection
         rented_games = self.game_collection.find({"_id": {"$in": rented_game_ids_list}})
 
-        
+        # Initialize an empty list to store all genres from the rented games
         genres = []
+
+        # Collect genres from each rented game 
         for game in rented_games:
             genres.extend(game.get("genres",[]))
 
-
+        # Initialize a dictionary to count the occurrence of each genre
         genre_counts = {}
         for genre in genres:
             if genre in genre_counts:
                 genre_counts[genre] += 1
             else:
                 genre_counts[genre] = 1
+
+        # Calculate the total number of genres to determine probabilities
         total_genres = len(genres)
+
+        # Compute the probability of each genre based on its occurrence
         genre_probabilities = {genre: count / total_genres for genre, count in genre_counts.items()}
         
-
+        # Randomly select one genre based on the calculated probabilities
         genre_selected = random.choices(
             population=list(genre_probabilities.keys()),
             weights=list(genre_probabilities.values()),
             k=1
         )[0]
 
+        # Define a MongoDB aggregation pipeline to find recommended games
         pipeline = [
             {"$match": {
-                #match games with the genre selected
+                # Match games with the genre selected
                 "genres": genre_selected,
-                #exclude games that already rented by the user
+                # Exclude games that already rented by the user
                 "_id": {"$nin": rented_game_ids_list}
             }},
             {"$sample": {"size": 5}}
         ]
 
+        # Execute the pipeline and get the recommended games
         recommend_games = list(self.game_collection.aggregate(pipeline))
 
+        # Return a list of titles for the recommended games
         return [game["title"] for game in recommend_games]
 
 
@@ -184,13 +197,13 @@ class DBManager:
         # Retrieve the updated user to check the rented_game_ids list
         updated_user = self.user_collection.find_one({"_id": user["_id"]})
 
-        #get the list of games rented by the user
+        # Get the list of games rented by the user
         rented_games_ids_list = updated_user.get("rented_game_ids",[])
 
         if not rented_games_ids_list:
             return ["No games rented"]
 
-        #pick random game from the user's rented games
+        # Pick random game from the user's rented games
         rented_games = []
         for game_id in rented_games_ids_list:
             game = self.game_collection.find_one({"_id": game_id})
@@ -198,21 +211,21 @@ class DBManager:
 
         selected_game = random.choice(rented_games)
 
-        #get the title of the selected game
+        # Get the title of the selected game
         selected_title = selected_game["title"]
 
-        #get all the games titles
+        # Get all the games titles
         games_db = list(self.game_collection.find({},{"title":1,"_id":0}))
         all_titles = [game["title"] for game in games_db]
 
-        #calculte TF-IDF for all the games titles
+        # Calculte TF-IDF for all the games titles
         vectorizer = TfidfVectorizer()
         tfidf_vectors = vectorizer.fit_transform(all_titles)
 
-        #find the position of the selected game title
+        # Find the position of the selected game title
         chosen_index = all_titles.index(selected_title)
 
-        #calculte cosine similarity between the selected game to the other games
+        # Calculte cosine similarity between the selected game to the other games
         cosine_similarities = cosine_similarity(tfidf_vectors[chosen_index], tfidf_vectors).flatten()
 
         sorted_score  = cosine_similarities.argsort()[::-1]
@@ -221,46 +234,60 @@ class DBManager:
             if all_titles[i] != selected_title:
                 similar_score.append(i)
 
-        #list of titles for games that the user rented
+        # List of titles for games that the user rented
         rented_titles = [game["title"] for game in rented_games]
-        #list of recommended games exclude games that the user rented
+        # List of recommended games exclude games that the user rented
         recommendations = [all_titles[i] for i in similar_score if all_titles[i] not in rented_titles]
 
         return recommendations[:5]
 
     def find_top_rated_games(self, min_score) -> list:
+        # Query the game collection to find games with a user score greater than or equal to min_score.
+        # Only return the "title" and "user_score" fields, excluding the "_id" field.
         top_rated_games = self.game_collection.find({"user_score": {"$gte": min_score}}, {"title": 1, "user_score": 1, "_id": 0})
         return list(top_rated_games)
 
     def decrement_scores(self, platform_name) -> None:
+        # Use the update_many operation to decrement the "user_score" field for all games matching the given platform name.
         self.game_collection.update_many(
             {"platform": platform_name},
             {"$inc": {"user_score": -1}}
         )
 
     def get_average_score_per_platform(self) -> dict:
-        platform_average = {}  # Dict
+        platform_average = {}  # Initialize an empty dictionary to store the results
+
+        # Define an aggregation pipeline to group games by platform and calculate the average user score for each group
         pl = [{
             "$group": {
                 "_id": "$platform",
                 "average_score": {"$avg": "$user_score"},
             }
         }]
+
+        # Execute the aggregation pipeline and convert the results to a list
         results = list(self.game_collection.aggregate(pl))
-        # Append results into dict
+
+        # Iterate over the results to populate the dictionary with platform names and their average scores
         for value in results:
             platform_average[value["_id"]] = round(value["average_score"], 3)
+
         return platform_average
 
     def get_genres_distribution(self) -> dict:
+        # Initialize an empty dictionary to store the genre distribution
         genres_dist = {}
+
+        # Define an aggregation pipeline to calculate the genre distribution
         pl = [
             {"$unwind": "$genres"},
             {"$group": {"_id": "$genres", "count": {"$sum": 1}}},
         ]
+
+        # Execute the aggregation pipeline and convert the results to a list
         results = list(self.game_collection.aggregate(pl))
 
-        # Append results into dict
+        # Iterate over the results to populate the dictionary with genre names and their counts
         for value in results:
             genres_dist[value["_id"]] = value["count"]
 
